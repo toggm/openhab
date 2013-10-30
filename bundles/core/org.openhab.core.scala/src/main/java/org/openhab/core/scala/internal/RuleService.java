a0bd3ff4bde8f92bdd4933cc31da7832c416c8eb
toggm <mike.toggweiler@tegonal.com>
2013-10-30 22:41:13 +0100

    created initial bundle to integrate scala based rule engine

---
+++ bundles/core/org.openhab.core.scala/src/main/java/org/openhab/core/scala/internal/RuleService.java
@@ -0,0 +1,199 @@
+/**
+ * Copyright (c) 2010-2013, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.core.scala.internal;
+
+import static org.openhab.core.events.EventConstants.TOPIC_PREFIX;
+import static org.openhab.core.events.EventConstants.TOPIC_SEPERATOR;
+
+import java.util.Collection;
+import java.util.Dictionary;
+
+import org.apache.commons.lang.StringUtils;
+import org.openhab.core.items.GenericItem;
+import org.openhab.core.items.Item;
+import org.openhab.core.items.ItemNotFoundException;
+import org.openhab.core.items.ItemRegistry;
+import org.openhab.core.items.ItemRegistryChangeListener;
+import org.openhab.core.items.StateChangeListener;
+import org.openhab.core.service.AbstractActiveService;
+import org.openhab.core.types.Command;
+import org.openhab.core.types.EventType;
+import org.openhab.core.types.State;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RuleService extends AbstractActiveService implements
+		ManagedService, EventHandler, ItemRegistryChangeListener,
+		StateChangeListener {
+
+	private static final String RULES_CHANGESET = "org/openhab/core/drools/changeset.xml";
+
+	static private final Logger logger = LoggerFactory
+			.getLogger(RuleService.class);
+
+	private ItemRegistry itemRegistry = null;
+
+	private long refreshInterval = 200;
+
+	public void activate() {
+
+		// now add all registered items to the session
+		if (itemRegistry != null) {
+			for (Item item : itemRegistry.getItems()) {
+				itemAdded(item);
+			}
+		}
+
+		setProperlyConfigured(true);
+	}
+
+	public void deactivate() {
+		shutdown();
+	}
+
+	public void setItemRegistry(ItemRegistry itemRegistry) {
+		this.itemRegistry = itemRegistry;
+		itemRegistry.addItemRegistryChangeListener(this);
+	}
+
+	public void unsetItemRegistry(ItemRegistry itemRegistry) {
+		itemRegistry.removeItemRegistryChangeListener(this);
+		this.itemRegistry = null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@SuppressWarnings("rawtypes")
+	public void updated(Dictionary config) throws ConfigurationException {
+		if (config != null) {
+			String evalIntervalString = (String) config.get("evalInterval");
+			if (StringUtils.isNotBlank(evalIntervalString)) {
+				refreshInterval = Long.parseLong(evalIntervalString);
+			}
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void allItemsChanged(Collection<String> oldItemNames) {
+
+		// first remove all previous items from the session
+		for (String oldItemName : oldItemNames) {
+			internalItemRemoved(oldItemName);
+		}
+
+		// then add the current ones again
+		Collection<Item> items = itemRegistry.getItems();
+		for (Item item : items) {
+			internalItemAdded(item);
+		}
+
+	}
+
+	private void internalItemAdded(Item item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void internalItemRemoved(String oldItemName) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void itemAdded(Item item) {
+		internalItemAdded(item);
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void itemRemoved(Item item) {
+		internalItemRemoved(item.getName());
+		if (item instanceof GenericItem) {
+			GenericItem genericItem = (GenericItem) item;
+			genericItem.removeStateChangeListener(this);
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void stateChanged(Item item, State oldState, State newState) {
+		//eventQueue.add(new StateEvent(item, oldState, newState));
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void stateUpdated(Item item, State state) {
+		//eventQueue.add(new StateEvent(item, state));
+	}
+
+	public void receiveCommand(String itemName, Command command) {
+		try {
+			Item item = itemRegistry.getItem(itemName);
+			//TODO eventQueue.add(new CommandEvent(item, command));
+		} catch (ItemNotFoundException e) {
+		}
+	}
+
+	/**
+	 * @{inheritDoc
+	 */
+	@Override
+	protected synchronized void execute() {
+		// remove all previous events from the session
+		
+//		ArrayList<RuleEvent> clonedQueue = new ArrayList<RuleEvent>(eventQueue);
+//		eventQueue.clear();
+	
+
+	}
+
+	@Override
+	protected long getRefreshInterval() {
+		return refreshInterval;
+	}
+
+	@Override
+	protected String getName() {
+		return "Rule Evaluation Service";
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void handleEvent(Event event) {
+		String itemName = (String) event.getProperty("item");
+
+		String topic = event.getTopic();
+		String[] topicParts = topic.split(TOPIC_SEPERATOR);
+
+		if (!(topicParts.length > 2) || !topicParts[0].equals(TOPIC_PREFIX)) {
+			return; // we have received an event with an invalid topic
+		}
+		String operation = topicParts[1];
+
+		if (operation.equals(EventType.COMMAND.toString())) {
+			Command command = (Command) event.getProperty("command");
+			if (command != null)
+				receiveCommand(itemName, command);
+		}
+	}
+}
